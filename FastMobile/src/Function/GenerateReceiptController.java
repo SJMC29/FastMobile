e545dd74172199eca1b0f619233c28573edeb848
Git-Fanfo <juanjosedoradom@gmail.com>
2022-03-10 18:32:41 -0500

    Pdf Template :D

--- FastMobile/src/Function/GenerateReceiptController.java
+++ FastMobile/src/Function/GenerateReceiptController.java
@@ -28,6 +28,10 @@ import com.itextpdf.text.DocumentException;
 import com.itextpdf.text.FontFactory;
 import com.itextpdf.text.Image;
 import com.itextpdf.text.Paragraph;
+import com.itextpdf.text.Phrase;
+import com.itextpdf.text.pdf.Barcode128;
+import com.itextpdf.text.pdf.PdfContentByte;
+import com.itextpdf.text.pdf.PdfPCell;
 import com.itextpdf.text.pdf.PdfPTable;
 import com.itextpdf.text.pdf.PdfWriter;
 import java.awt.Font;
@@ -80,29 +84,97 @@ public class GenerateReceiptController {
     }
     
     public void createPdf() throws FileNotFoundException, DocumentException, BadElementException, IOException{
-        
-        
-        
-//Image logo = "src\\Images\\Login\\FastMobileWhite.png";
+        //Image logo = "src\\Images\\Login\\FastMobileWhite.png";
         Document documento = new Document();
         FileOutputStream ficheroPDF = new FileOutputStream("src\\Pdf\\hello_world.pdf");
-        PdfWriter.getInstance(documento, ficheroPDF);
+        PdfWriter pdfWriter = PdfWriter.getInstance(documento, ficheroPDF);
         documento.open();
         
+        //if you would have a chapter indentation
+        int indentation = 0;
         Image logo = Image.getInstance("src\\Images\\Menu\\FastMobileBlue.png");
+        float scaler = ((documento.getPageSize().getWidth() - documento.leftMargin()
+               - documento.rightMargin() - indentation) / logo.getWidth()) * 100;
+        logo.scalePercent(scaler);
+        
         documento.add(logo);
-        Paragraph titulo = new Paragraph("lista de personas \n\n",FontFactory.getFont("arial",22,Font.BOLD,BaseColor.BLUE));        
+        Paragraph titulo = new Paragraph("Fast Mobile: La mayor cobertura en el menor tiempo.\n",FontFactory.getFont("arial",14,Font.BOLD,new BaseColor(41, 135, 217)));        
         documento.add(titulo);
         
-        PdfPTable tabla = new PdfPTable(3);
+        documento.add(new Phrase("\nCliente ",FontFactory.getFont("arial",11,Font.BOLD)));
+        documento.add(new Phrase("-----"));
+        documento.add(new Phrase("\nDirección ",FontFactory.getFont("arial",11,Font.BOLD)));
+        documento.add(new Phrase("-----"));
+        documento.add(new Phrase("\nCédula ",FontFactory.getFont("arial",11,Font.BOLD)));
+        documento.add(new Phrase("-----"));
+        documento.add(new Phrase("\nTeléfono ",FontFactory.getFont("arial",11,Font.BOLD)));
+        documento.add(new Phrase("-----"));
+        documento.add(new Phrase("\nFecha de expedición ",FontFactory.getFont("arial",11,Font.BOLD)));
+        documento.add(new Phrase("-----"));
+        documento.add(new Phrase("\nFactura de venta No ",FontFactory.getFont("arial",11,Font.BOLD)));
+        documento.add(new Phrase("-----"));
         
-        tabla.addCell("ID");
-        tabla.addCell("NOMBRE");
-        tabla.addCell("TELEFONO");
-        
+        PdfPTable tabla = new PdfPTable(2);
+        PdfPCell cell = new PdfPCell(new Phrase ("Periodo de facturación",FontFactory.getFont("arial",11,Font.BOLD)));
+        cell.setBackgroundColor(new BaseColor(41, 135, 217));
+        cell.setPaddingBottom(5);
+        tabla.addCell(cell);
+        tabla.addCell("-----");
+        cell.setPhrase(new Phrase ("Fecha límite de pago",FontFactory.getFont("arial",11,Font.BOLD)));
+        tabla.addCell(cell);
+        tabla.addCell("-----");
+        cell.setPhrase(new Phrase ("Total a pagar",FontFactory.getFont("arial",11,Font.BOLD)));
+        tabla.addCell(cell);
+        tabla.addCell("-----");
         documento.add(tabla);
         
-        
+        documento.add(new Phrase(" "));
+        tabla = new PdfPTable(1);
+        //cell.setPhrase(new Phrase ("Estimado cliente, pague oportunamente y evite la suspensión del servicio, cobro de reconexión por producto e intereses de mora: El incumplimiento en los pagos genera reportes a Centrales de Riesgo. Si ya realizó el pago, haga caso omiso.",FontFactory.getFont("arial",12,Font.BOLD)));
+        cell.setPhrase(new Phrase ("Estimado cliente, pague oportunamente y evite la suspensión del servicio, cobro de reconexión por producto e intereses de mora: El incumplimiento en los pagos genera reportes a Centrales de Riesgo como moroso. Si ya realizó el pago, haga caso omiso.",FontFactory.getFont("arial",11)));
+        cell.setPaddingBottom(20);
+        cell.setPaddingLeft(10);
+        cell.setPaddingRight(10);
+        cell.setPaddingTop(10);
+        cell.setBackgroundColor(new BaseColor(242, 229, 46));
+        tabla.addCell(cell);
+        documento.add(tabla);
+
+        documento.add(new Phrase(" "));
+        tabla = new PdfPTable(3);  
+        cell.setPhrase(new Phrase ("LINEA ASOCIADA",FontFactory.getFont("arial",11,Font.BOLD)));
+        cell.setPaddingBottom(5);
+        cell.setPaddingLeft(5);
+        cell.setPaddingRight(5);
+        cell.setPaddingTop(5);
+        cell.setBackgroundColor(new BaseColor(41, 135, 217));
+        tabla.addCell(cell);
+        cell.setPhrase(new Phrase ("PLAN",FontFactory.getFont("arial",11,Font.BOLD)));
+        tabla.addCell(cell);
+        cell.setPhrase(new Phrase ("MINUTOS",FontFactory.getFont("arial",11,Font.BOLD)));
+        tabla.addCell(cell);
+        //Espacio para el for    
+        tabla.addCell("31454312");
+        tabla.addCell("Ilimitado");
+        tabla.addCell("72");
+        tabla.addCell("31454312");
+        tabla.addCell("Ilimitado");
+        tabla.addCell("72");
+        tabla.addCell("31454312");
+        tabla.addCell("Ilimitado");
+        tabla.addCell("72");
+        documento.add(tabla);
+                
+        documento.add(new Phrase("\n"));
+        Barcode128 barcode128 = new Barcode128();
+        barcode128.setCode("(415)7707247180153(8021)0001006106500(3902)000000058000(96)20220310");//(415)7707247180153(8021)ID(3902)VALOR(96)PERIODO
+        barcode128.setCodeType(Barcode128.CODE128);
+        PdfContentByte pdfContentByte = pdfWriter.getDirectContent();
+        Image code128Image = barcode128.createImageWithBarcode(pdfContentByte, null, null);
+        //code128Image.setAbsolutePosition(10, 700);
+        code128Image.scalePercent(100);
+        code128Image.setAlignment(Image.MIDDLE);
+        documento.add(code128Image);
         //PdfPTable table = PdfPTable();
         
         documento.close();
--- FastMobile/src/Interface/GenerateReceipt.java
+++ FastMobile/src/Interface/GenerateReceipt.java
@@ -12,9 +12,13 @@ import Function.ProfileClientsController;
 import Function.RegisterClientController;
 import Models.Client;
 import Models.User;
+import com.itextpdf.text.DocumentException;
 import java.awt.Color;
 import java.awt.Cursor;
 import java.awt.Image;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import javax.swing.ImageIcon;
 
@@ -313,10 +317,16 @@ public class GenerateReceipt extends javax.swing.JFrame {
     }//GEN-LAST:event_buscarCliente
 
     private void registrarPago(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_registrarPago
-        // TODO add your handling code here:
-        //controladorGenerateReceipt.actualizarFecha(lineasTelefonicas, cliente);
-        //seAgrego.setVisible(true);
-        controladorGenerateReceipt.createPdf();
+        try {
+            // TODO add your handling code here:
+            //controladorGenerateReceipt.actualizarFecha(lineasTelefonicas, cliente);
+            //seAgrego.setVisible(true);
+            controladorGenerateReceipt.createPdf();
+        } catch (DocumentException ex) {
+            Logger.getLogger(GenerateReceipt.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IOException ex) {
+            Logger.getLogger(GenerateReceipt.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }//GEN-LAST:event_registrarPago
 
     private void menuMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_menuMouseClicked
@@ -336,63 +346,9 @@ public class GenerateReceipt extends javax.swing.JFrame {
      * @param args the command line arguments
      */
     public static void main(String args[]) {
-        /* Set the Nimbus look and feel */
-        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
-        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
-         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
-         */
-        try {
-            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-                if ("Nimbus".equals(info.getName())) {
-                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
-                    break;
-                }
-            }
-        } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(GenerateReceipt.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(GenerateReceipt.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(GenerateReceipt.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(GenerateReceipt.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        }
-        //</editor-fold>
-        //</editor-fold>
-        //</editor-fold>
-        //</editor-fold>
-        //</editor-fold>
-        //</editor-fold>
-        //</editor-fold>
-        //</editor-fold>
-        //</editor-fold>
-        //</editor-fold>
-        //</editor-fold>
-        //</editor-fold>
-        //</editor-fold>
-        //</editor-fold>
-        //</editor-fold>
-        //</editor-fold>
-        //</editor-fold>
-        //</editor-fold>
-        //</editor-fold>
-        //</editor-fold>
-        //</editor-fold>
-        //</editor-fold>
-        //</editor-fold>
-        //</editor-fold>
-        //</editor-fold>
-        //</editor-fold>
-        //</editor-fold>
-        //</editor-fold>
-        //</editor-fold>
-        //</editor-fold>
-        //</editor-fold>
-        //</editor-fold>
-
-        /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
+                
                 new GenerateReceipt().setVisible(true);
             }
         });
